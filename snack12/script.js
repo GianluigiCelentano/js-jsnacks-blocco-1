709f1e4a43dbb0fcfcf08f4b2b5ec9e71910a9a9
GianluigiCelentano <cgianluigi96@gmail.com>
2021-06-04 16:41:38 +0200

    js-jsnacks-blocco-1

--- snack12/script.js
+++ snack12/script.js
@@ -1,8 +1,7 @@
 var surnames = ["Bianchi","Conti","Rovesi","Molli","Pinzi"]
 var names = ["Girolamo","Settembrino","Osvaldo","Ajeje","Gino"]
-var randomNames = names[Math.floor(Math.random()*names.length)]
-var randomSurnames = surnames[Math.floor(Math.random()*surnames.length)]
-for(i=0; i<=3; i++) {
-    document.getElementById("names").innerHTML = randomNames
-    document.getElementById("surnames").innerHTML = randomSurnames
+for(i=1; i<=3; i++) {
+    var randomNames = names[Math.floor(Math.random()*names.length)]
+    var randomSurnames = surnames[Math.floor(Math.random()*surnames.length)]
+    document.getElementById("names").innerHTML += "<br>" + randomNames + " " + randomSurnames
 }
