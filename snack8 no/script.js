bca18edebc73a90d095e1324efecfbee5d85ea8f
GianluigiCelentano <cgianluigi96@gmail.com>
2021-06-04 17:14:55 +0200

    js-jsnacks-blocco-1

--- snack8 no/script.js
+++ snack8 no/script.js
@@ -1,11 +1,18 @@
 var askNumber = prompt("inserisci un numero a 4 cifre");
-if (askNumber.length !== 4) {
-    alert("inserisci un numero a 4 cifre")
-} else if ((isNaN) askNumber) {
-    alert ("inserisci un numero")
+var sum = 0
+// if (askNumber.length !== 4) {
+//     alert("inserisci un numero a 4 cifre")
+// } else if (isNaN(askNumber)) {
+//     alert ("inserisci un numero")
+// }
+// else {
+//     // askNumber = 2345
+while (askNumber.length < 4) {
+    askNumber = prompt("inserisci un numero a 4 cifre");
 }
-else {
 for ( i = 0; i < askNumber.length; i++) {
-    document.getElementById("numbers").innerHTML+= parseInt(askNumber[i])
+    sum += parseInt(askNumber[i])
 }
-}
+document.getElementById("numbers").innerHTML += sum
+// }
+
